2aa11b8f6f580173b7d1d6dbbde952c8a48ce35a
Wiktor Andrzejkiewicz <wiktor.andrzejkiewicz@gmail.com>
2023-04-14 18:19:03 +0200

    Initial commit

---
+++ src/Animal.java
@@ -0,0 +1,22 @@
+public class Animal {
+    String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
